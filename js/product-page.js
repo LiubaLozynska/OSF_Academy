d211a8aefb6966c10ba8882859d8527e7200bbbf
Liuba Lozynska <liuba.lozynska@gmail.com>
2020-05-19 17:51:25 +0300

    Product page layout and responsive styles

--- js/product-page.js
+++ js/product-page.js
@@ -1,22 +1,63 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  nav:false,
-  dots:true,
-  margin:10,
-  responsiveClass:true,
-  responsive:{
-      0:{
+$('#product-preview').owlCarousel({
+    loop:true,
+    nav:false,
+    dots:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:3,
+        },
+    }
+  }) 
+
+  $('#popular-products').owlCarousel({
+    loop:true,
+    nav:false,
+    dots:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        360:{
+          items:2,
+        },
+        768:{
           items:3,
-          nav:true
-      },
-      600:{
-          items:3,
-          nav:false
-      },
-      1000:{
-          items:5,
-          nav:true,
-          loop:false
+        },
+        980:{
+          items:4,
+        },
+
+    }
+  })
+
+
+let tabs = $('.tabs__item');
+let tabsCount = tabs.length;
+
+
+for (let i=0; i < tabsCount; i++) {
+    tabs.eq(i).on('click', (event)=> {
+
+      let ID = event.target.id;
+      let stringID = '#' + event.target.id;
+
+      $(stringID).toggleClass('active');
+      $("div[data-show=" + ID + "]").toggleClass('show');
+
+      for (let i=0; i < tabsCount; i++) {
+        
+        if (tabs.eq(i)[0].id !== ID) {
+          let idToHide = tabs.eq(i)[0].id;
+          let stringIDtoHide = '#' + tabs.eq(i)[0].id;
+
+          $(stringIDtoHide).removeClass('active');
+          $("div[data-show=" + idToHide + "]").removeClass('show');
+
+        }
       }
-  }
-})
+    })
+}
